c6da16e65820af8d5b85520568c6adc42d37871b
kleper240 <mabulu.marius33@gmail.com>
2023-12-31 22:15:02 +0100

    dans le fichier app checkWin, chekDraw ok

--- app.js
+++ app.js
@@ -43,12 +43,36 @@ function MakeMove(row, col){
     }
 }
 
-function checkWin(){
+function checkWin(player){
+
+    if (board[0][0] === player &&  board[1][1] === player && board[2][2] === player  || board[0][2] === player &&  board[1][1] === player && board[2][0] === player){
+        return true;
+    }
+
+    for (let row = 0 ; row < 3; row++){
+        if (board[row][0] === player && board[row][1] === player  && board[row][2] === player){
+            return true;
+        }
+    }
+    for (let col = 0 ; col < 3; col++){
+        if (board[0][col] === player && board[1][col] === player  && board[2][col] === player){
+            return true;
+        }
+    }
+    return false;
 
 }
 
 function chekDraw(){
-    
+    for (let row = 0 ; row < 3 ; row++){
+        for (let col = 0; col < 3; col ++){
+            if (board[row][col] === player){
+                return true;
+            }
+        }
+    }
+    return false;
+
 }
 
 function resetGame(){
